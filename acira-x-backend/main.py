c1c1dca31f02c36d0500ef620ab784be21aecba8
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2026-06-19 23:01:01 +0530

    Update main.py

--- acira-x-backend/main.py
+++ acira-x-backend/main.py
@@ -1,8 +1,3 @@
-from database import engine
-import models
-
-models.Base.metadata.create_all(bind=engine)
-
 from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
